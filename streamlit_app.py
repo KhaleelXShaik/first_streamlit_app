094c6fc2e0a8b2cafb8f711acc13c0eec7a3f553
KhaleelShaik <120924112+KhaleelXShaik@users.noreply.github.com>
2022-12-19 21:29:30 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,7 +33,7 @@ else:
     fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
     streamlit.dataframe(fruityvice_normalized)
    
-export URLErroras e:
+export URLError as e:
   streamlit.error()
   
 streamlit.stop()
